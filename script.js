365d04aa391e9c36096dbade813cb2267b5aa8d5
Abdul Rouf Shaik <abdulroufshaik123@gmail.com>
2024-04-08 13:22:21 +0530

    added page3 and made page2, page3 responsive

--- script.js
+++ script.js
@@ -15,15 +15,16 @@ clr.addEventListener("click", () => {
 });
 
 
-gsap.to("#page2 .left img,#page2 .right img ", {
-    transform: "scale(1.6)",
+gsap.to(".background", {
+    scale: 1.3,
     scrollTrigger: {
         // markers: true,
         trigger: "#page2",
         start: "top 80%",
         end: "bottom 10%",
-        scrub: 2
+        scrub: 1.3
     }
 })
+
 
 
